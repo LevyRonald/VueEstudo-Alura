3b1ee12571d198477ca125b2a284f6fce229c151
Levy <levy.ronald@tallos.com.br>
2022-03-15 11:52:12 -0300

    att: aula 02 (concluída)

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+<div>
+  <h1>{{ Titulo }}</h1>
 
+  <ul>
+    <li v-for="foto of fotos">
+      <img :src="foto.url" :alt="foto.titulo">
+      </li>
+  </ul>
+</div>
+</template>
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+
+     data(){
+     
+      return{
+        Titulo: "Alurapic",
+
+        fotos: [
+          {
+            url: 'https://folhago.com.br/blogs/my-pet/wp-content/uploads/2021/11/Design-sem-nome-22.png',
+            titulo: 'cachorro cururu'
+          },
+          {
+            url: 'https://folhago.com.br/blogs/my-pet/wp-content/uploads/2021/11/Design-sem-nome-22.png',
+            titulo: 'cururu cachorro'
+          }
+        ]
+     }
   }
 }
 </script>
-
+ 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
